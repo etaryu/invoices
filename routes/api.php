d3572c4434c40a97ad82868d637521c4a1fc2975
yuggo <yuggoteruya1@gmail.com>
2025-02-27 09:12:30 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\v1\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/users', [UserController::class, 'index']);
